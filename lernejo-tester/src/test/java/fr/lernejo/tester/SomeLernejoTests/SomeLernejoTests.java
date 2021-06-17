f1fa12145035b0615d04e0585170da3d256171ef
Marius Mihai <marius.mihai@epita.fr>
2021-06-17 12:30:20 +0200

    etape 3 fini

---
+++ lernejo-tester/src/test/java/fr/lernejo/tester/SomeLernejoTests/SomeLernejoTests.java
@@ -0,0 +1,15 @@
+package fr.lernejo.tester.SomeLernejoTests;
+
+import fr.lernejo.tester.api.TestMethod;
+
+public class SomeLernejoTests {
+    @TestMethod
+    public void ok(){
+
+    }
+    @TestMethod
+    public void ko() {throw  new IllegalStateException();}{
+    }
+    public void none(){
+    }
+}
